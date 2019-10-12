f530f55ff781d244bf39b572a85f1c4fa54bc782
Max Vizard <maxvizard@gmail.com>
2019-10-12 12:31:16 +0100

    Fix bugs in game

--- libs/game.py
+++ libs/game.py
@@ -124,9 +124,9 @@ class Game():
         self.ball.update()
 
     def collision(self, rect1, rect2):
-    # note this function doesn't use the self parameter so it could be static
-    # or defined outside the Game class if we wanted.
-    return (rect1.x < rect2.x + rect2.width &&
-            rect1.x + rect1.width > rect2.x &&
-            rect1.y < rect2.y + rect2.height &&
+        # note this function doesn't use the self parameter so it could be static
+        # or defined outside the Game class if we wanted.
+        return (rect1.x < rect2.x + rect2.width and
+            rect1.x + rect1.width > rect2.x and
+            rect1.y < rect2.y + rect2.height and
             rect1.y + rect1.height > rect2.y)
--- part3.rst
+++ part3.rst
@@ -229,9 +229,9 @@ objects and checks if they collide::
     def collision(self, rect1, rect2):
         # note this function doesn't use the self parameter so it could be static
         # or defined outside the Game class if we wanted.
-        return (rect1.x < rect2.x + rect2.width &&
-                rect1.x + rect1.width > rect2.x &&
-                rect1.y < rect2.y + rect2.height &&
+        return (rect1.x < rect2.x + rect2.width and
+                rect1.x + rect1.width > rect2.x and
+                rect1.y < rect2.y + rect2.height and
                 rect1.y + rect1.height > rect2.y)
 
 Now we need to call this fucntion on the ball and other objects on every update.
